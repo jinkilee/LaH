99b0c8f3a55433f75d6db71ed37fbd40d7304bb0
Jinki Lee <jinkilee73@gmail.com>
2020-03-13 17:51:31 +0900

    edit transformer

--- modeling/transformer.py
+++ modeling/transformer.py
@@ -20,8 +20,7 @@ class EncoderDecoder(nn.Module):
 		
 	def forward(self, src, tgt, src_mask, tgt_mask):
 		"Take in and process masked src and target sequences."
-		return self.decode(self.encode(src, src_mask), src_mask,
-							tgt, tgt_mask)
+		return self.decode(self.encode(src, src_mask), src_mask, tgt, tgt_mask)
 	
 	def encode(self, src, src_mask):
 		return self.encoder(self.src_embed(src), src_mask)
